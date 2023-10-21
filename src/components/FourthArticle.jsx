ef77e905570ba7800532f746ac2859cf17e9ce13
Fran Lopez <elsilencio_f@hotmail.com>
2023-10-21 20:04:46 -0300

    fourth changed

--- src/components/FourthArticle.jsx
+++ src/components/FourthArticle.jsx
@@ -31,13 +31,13 @@ const FourthArticle = () => {
           </p>
           <div className="flex justify-center items-center gap-10">
             <a
-              href="https://estudio-juridico-beta.web.app/"
+              href="https://github.com/Fran249/Estudio-juridico"
               className="hover:translate-y-[-2px] transition-transform active:translate-y-[2px]"
             >
               <GitHubIcon tailwindClass={"w-6 h-6 text-white"} />
             </a>
             <a
-              href="https://github.com/Fran249/Estudio-juridico"
+              href="https://estudio-juridico-beta.web.app/"
               className="hover:translate-y-[-2px] transition-transform active:translate-y-[2px]"
             >
               <ExternalLinkIcon color={"white"} />
